f4deebebc1e028eaf8566a586c44c236c307d10a
Saeideh Tajmehr <saeideh.tajmehr@efsibiz.com>
2025-02-17 15:30:28 +0330

    mongo debug

--- routes/user.js
+++ routes/user.js
@@ -53,6 +53,7 @@ router.get("/", async(req, res)=>{
     const query = req.query.new
     try{
         const users = query ? await User.find().sort({_id:-1}).limit(5) : await User.find()
+       console.log('user run ')
         res.status(200).json(users)
     }
     catch(err){
